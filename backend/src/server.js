15fda272b2080d74069b3f6be50ddc7e196d3d59
Surbhi07-23 <jhasurbhi08@gmail.com>
2026-01-13 13:34:00 +0530

    deployment section added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,27 @@
 import express from "express";
+import path from "path";
 import {ENV} from "./lib/env.js";
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+const __dirname = path.resolve();  //path.resolve() Returns the absolute path of the current working directory
 
-app.get("/" , (req ,res) => {
-    res.status(200).json({msg : "Success from backend"})
+app.get("/health" , (req ,res) => {
+    res.status(200).json({msg : "app is running fine"})
+})
+
+app.get("/books" , (req ,res) => {
+    res.status(200).json({msg : "books endpoint"})
+})
+
+//making our app ready for deployment
+if(ENV.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname , "../frontend/dist")));   
+    // __dirname = backend    ".. " will move it one up that is to intervue folder --> then to frontend and then dist
+}
+
+app.get("/{*any}" , (req , res)=>{    //if any other endpoint other then mentioned here , display the react app
+    res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"))
 })
 
 app.listen(ENV.PORT, ()=>{
