f44179a20d9999448a2286d58ec52226f5b9ab07
Surbhi07-23 <jhasurbhi08@gmail.com>
2026-06-22 20:50:20 +0530

    validation chcek added for DB_URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,7 +4,7 @@ import {ENV} from "./env.js"
 
 const connectDB = async() => {
     try{
-        if(!ENV.DB_URL){
+        if (!ENV.DB_URL) {
             throw new Error("DB_URL is not defined in environment variables")
         }
         const conn = await mongoose.connect(ENV.DB_URL)
