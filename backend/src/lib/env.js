e21ca08dddf8ee4bd0c9b6a356322718d51ac1a1
Surbhi07-23 <jhasurbhi08@gmail.com>
2026-06-23 11:18:36 +0530

    auth and db sync

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,5 +5,10 @@ dotenv.config();
 export const ENV = {            //key-value pair
     PORT : process.env.PORT,
     DB_URL : process.env.DB_URL,
-    NODE_ENV : process.env.NODE_ENV
+    NODE_ENV : process.env.NODE_ENV,
+    CLIENT_URL: process.env.CLIENT_URL,
+    INGGEST_EVENT_KEY: process.env.INGGEST_EVENT_KEY,
+    INGGEST_SIGNING_KEY : process.env.INGGEST_SIGNING_KEY,
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+    STREAM_API_SECRET : process.env.STREAM_API_SECRET
 } 
---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,44 @@
+//if new user logs in using clerk this will add that user in mongoose database    same for delete
+
+import {Inngest} from inngest;
+import connectDB from "../src/lib/db.js";
+import User from "../models/User.js"
+
+export const inngest = new Inngest({idd : "prepify"});
+
+const syncUser =    inngest.createFunction(
+    {id:"sync-user"},
+    {event:"cleark/user.created"},
+    async({event}) => {
+        await connectDB();
+
+        const {id , email_addresses , first_name , last_name , image_url} = event.data
+
+        const newUser = {
+            clerkId:id,
+            email:email_addresses[0]?.email_address,
+            name:`${first_name || ""} ${last_name  || ""}` ,
+            profileImage:image_url
+        }
+
+        await User.create(newUser)
+
+        //todo
+    }
+)
+
+const deleteUserFromDB =    inngest.createFunction(
+    {id:"delete-user-from-db"},
+    {event:"cleark/user.deleted"},
+    async({event}) => {
+        await connectDB();
+
+        const {id} = event.data
+
+        await User.deleteOne({clerkId:id})
+
+        //todo
+    }
+)
+
+export const functions = [syncUser , deleteUserFromDB]
